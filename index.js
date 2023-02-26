4e276fd6efb74afd9ff7b9f5f294c4714c53bf77
Sam Dingore <55233678+SamDingore@users.noreply.github.com>
2023-02-26 18:48:51 +0530

    testing apoi

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get('/sam', (req, res) => {
+    const key = req.pass;
+    if (key=="2550") {
+        res.send("auth completed");
+    } else {
+        res.send("auth failed");
+    }
+})
 app.listen(process.env.PORT || 3000)
