4dee335f1decb11eff8bce7d60e42d05a1b086fe
Sam Dingore <55233678+SamDingore@users.noreply.github.com>
2023-03-26 21:21:11 +0530

    teset chat

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+let buffer =[];
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
@@ -7,10 +8,13 @@ app.all('/', (req, res) => {
 
 app.get('/sam', (req, res) => {
     const key = req.query.pass;
-    if (key=="2550") {
-        res.send('auth successful');
-    } else {
-        res.send('auth failed');
-    }
+    buffer.push(key);
+    // if (key=="2550") {
+    //     res.send('auth successful');
+    // } else {
+    //     res.send('auth failed');
+    // }
+
+    res.send(buffer[buffer.length-1]);
 })
 app.listen(process.env.PORT || 3000)
